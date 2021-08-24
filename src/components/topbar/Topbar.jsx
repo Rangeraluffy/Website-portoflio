a79ecf0f90acfa9d0b05fa5602205af189023c93
Alexandre MAROLLEAU <alexandre.marolleau@hotmail.fr>
2021-08-24 20:21:04 +0200

    Add logo and css

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -3,7 +3,15 @@ import "./topbar.scss"
 export default function Topbar() {
   return (
     <div className="topbar">
+      <div className="wrapper">
+        <div className="left">
+            <a href="#intro" className="logo">Alexandre Marolleau</a>
+            <div className="itemContainer">
 
+            </div>
+        </div>
+        <div className="right">This is right</div>
+      </div>
     </div>
   )
 }
