d48084c71ba7a3f0a129850f38bdb666f49b43ff
Alexandre MAROLLEAU <alexandre.marolleau@hotmail.fr>
2021-08-25 06:42:57 +0200

    Using Material-UI Icons and add weight

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,4 +1,5 @@
 import "./topbar.scss"
+import {Person, Mail} from '@material-ui/icons/';
 
 export default function Topbar() {
   return (
@@ -7,10 +8,15 @@ export default function Topbar() {
         <div className="left">
             <a href="#intro" className="logo">Alexandre Marolleau</a>
             <div className="itemContainer">
-
+              <Person className="icon"/>
+              <span>+33 6 59 08 60 95</span>
+            </div>
+            <div className="itemContainer">
+              <Mail className="icon"/>
+              <span>alexandre.marolleau@hotmail.fr</span>
             </div>
         </div>
-        <div className="right">This is right</div>
+        <div className="right"></div>
       </div>
     </div>
   )
