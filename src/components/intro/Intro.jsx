0885d89a4ffecbf132a63f3a9c8a1f4ef30b3a2d
Alexandre MAROLLEAU <alexandre.marolleau@hotmail.fr>
2021-08-25 22:45:05 +0200

    Add introduction page HTML & CSS

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,7 +3,21 @@ import "./intro.scss"
 export default function Intro() {
   return (
     <div className="intro" id="intro">
-
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/profil.png" alt=""/>
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Alexandre Marolleau</h1>
+          <h3>Student Web Developper<span></span></h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
     </div>
   )
 }
