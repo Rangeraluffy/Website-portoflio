442d318f39444a75dc2a1bbc5d0a159845be624f
Alexandre MAROLLEAU <alexandre.marolleau@hotmail.fr>
2021-08-25 23:10:12 +0200

    Text typing animation

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,6 +1,30 @@
 import "./intro.scss"
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 2500,
+      backSpeed:40,
+      strings: ["Meticulous", "Independent", "Skilful", "Enthusiast", "Passionate"],
+    });
+  }, []);
+
+  const textRef2 = useRef();
+
+  useEffect(() => {
+    init(textRef2.current, {
+      showCursor: true,
+      backDelay: 3000,
+      backSpeed:300,
+      strings: ["EPFL", "Lausanne"],
+    });
+  }, []);
+
   return (
     <div className="intro" id="intro">
       <div className="left">
@@ -12,7 +36,8 @@ export default function Intro() {
         <div className="wrapper">
           <h2>Hi There, I'm</h2>
           <h1>Alexandre Marolleau</h1>
-          <h3>Student Web Developper<span></span></h3>
+          <h3>Student <span ref={textRef}></span></h3>
+          <h3>Learn at <span ref={textRef2}></span></h3>
         </div>
         <a href="#portfolio">
           <img src="assets/down.png" alt="" />
