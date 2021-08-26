def707dcecd54d06bc96693a712309d5f992c506
Alexandre MAROLLEAU <alexandre.marolleau@hotmail.fr>
2021-08-26 18:16:22 +0200

    Add dynamic data with useEffect

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,32 +1,53 @@
 import { useEffect, useState } from "react";
 import "./portfolio.scss"
 import PortfolioList from "../portfolioList/PortfolioList";
+import {
+  allProject,
+  webPortfolio,
+  personalProject,
+  schoolProject,
+} from "../../data";
 
 export default function Portfolio() {
   const [selected, setSelected] = useState("featured");
   const [data, setData] = useState([]);
   const list = [
     {
-      id: "featured",
-      title: "Featured",
+      id: "all",
+      title: "All project",
     },
     {
       id: "web",
       title: "Web App",
     },
     {
-      id: "mobile",
-      title: "Mobile App",
-    },
-    {
       id: "design",
       title: "Personal Project",
     },
     {
-      id: "content",
+      id: "school",
       title: "School project",
     },
   ];
+
+  useEffect(() => {
+    switch (selected) {
+      case "all":
+        setData(allProject);
+        break;
+      case "web":
+        setData(webPortfolio);
+        break;
+      case "design":
+        setData(personalProject);
+        break;
+      case "school":
+        setData(schoolProject);
+        break;
+      default:
+        setData(allProject);
+    }
+  }, [selected]);
 
   return (
     <div className="portfolio" id="portfolio">
@@ -42,26 +63,15 @@ export default function Portfolio() {
         ))}
       </ul>
       <div className="container">
-        <div className="item">
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&uspq=CAU" alt=""/>
-          <h3>Project</h3>
-        </div>
-        <div className="item">
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&uspq=CAU" alt=""/>
-          <h3>Project</h3>
-        </div>
-        <div className="item">
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&uspq=CAU" alt=""/>
-          <h3>Project</h3>
-        </div>
-        <div className="item">
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&uspq=CAU" alt=""/>
-          <h3>Project</h3>
-        </div>
-        <div className="item">
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&uspq=CAU" alt=""/>
-          <h3>Project</h3>
-        </div>
+        {data.map((d) => (
+          <div className="item">
+            <img
+              src={d.img}
+              alt=""
+            />
+            <h3>{d.title}</h3>
+          </div>
+        ))}
       </div>
     </div>
   )
