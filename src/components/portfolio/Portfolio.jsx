d4f9a4c6c9c51004f99a53803ab1a4e5a034eb1b
Alexandre MAROLLEAU <alexandre.marolleau@hotmail.fr>
2021-08-26 10:41:57 +0200

    Create Portfolio page

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,36 @@ import "./portfolio.scss"
 export default function Portfolio() {
   return (
     <div className="portfolio" id="portfolio">
-
+      <h1>Portfolio</h1>
+      <ul>
+        <li className="active">Featured</li>
+        <li>Web App</li>
+        <li>Mobile App</li>
+        <li>Personal Project</li>
+        <li>Project School</li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&uspq=CAU" alt=""/>
+          <h3>Project</h3>
+        </div>
+        <div className="item">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&uspq=CAU" alt=""/>
+          <h3>Project</h3>
+        </div>
+        <div className="item">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&uspq=CAU" alt=""/>
+          <h3>Project</h3>
+        </div>
+        <div className="item">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&uspq=CAU" alt=""/>
+          <h3>Project</h3>
+        </div>
+        <div className="item">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&uspq=CAU" alt=""/>
+          <h3>Project</h3>
+        </div>
+      </div>
     </div>
   )
 }
