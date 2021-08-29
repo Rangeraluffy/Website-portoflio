e2f943ec6c98b19162b3244e8b1c361f0427e09b
Alexandre MAROLLEAU <alexandre.marolleau@hotmail.fr>
2021-08-29 18:25:15 +0200

    React Slider

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,9 +1,67 @@
+import { useState } from "react";
 import "./works.scss"
 
 export default function Works() {
+
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const data = [
+    {
+      id: 1,
+      title: "Web Social Media App",
+      img: "https://cdn.dribbble.com/users/5031392/screenshots/15467520/media/c36b3b15b25b1e190d081abdbbf947cf.png?compress=1&resize=1200x900",
+      desc:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    },
+    {
+      id: 2,
+      title: "Web Rampa UI Design",
+      img: "https://cdn.dribbble.com/users/3967258/screenshots/15463803/media/4fddb9a2caf3b3bd634060f706a91e73.png?compress=1&resize=1200x900",
+      desc:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    },
+    {
+      id: 3,
+      title: "Web E-commerce Design",
+      img: "https://cdn.dribbble.com/users/3637068/screenshots/15467010/media/69a3279182b75365013fe285733d1122.png?compress=1&resize=1200x900",
+      desc:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    },
+  ];
+
+  const handleClick = (way) => {
+    way === "left"
+      ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 2)
+      : setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0);
+  };
+
   return (
     <div className="works" id="works">
-
+      <div className="slider" style={{ transform: `translateX(-${currentSlide * 100}vw)` }}>
+        {data.map((d) => (
+          <div className="container">
+            <div className="item">
+              <div className="left">
+                <div className="leftContainer">
+                  <div className="imgContainer">
+                    <img src={d.img} alt="" />
+                  </div>
+                  <h2>{d.title}</h2>
+                  <p>{d.desc}</p>
+                  <span>Projects</span>
+                </div>
+              </div>
+              <div className="right">
+                <img
+                  src="https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <img src="assets/arrow.png" className="arrow left" alt="" onClick={() => handleClick("left")}/>
+      <img src="assets/arrow.png" className="arrow right" alt="" onClick={() => handleClick()}/>
     </div>
-  )
+  );
 }
