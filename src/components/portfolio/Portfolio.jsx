ef9e8120a2b4c3a9278dd3492968295124844b34
Alexandre MAROLLEAU <alexandre.marolleau@hotmail.fr>
2021-10-20 21:07:21 +0200

    Portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,78 +1,18 @@
-import { useEffect, useState } from "react";
 import "./portfolio.scss"
-import PortfolioList from "../portfolioList/PortfolioList";
-import {
-  allProject,
-  webPortfolio,
-  personalProject,
-  schoolProject,
-} from "../../data";
 
-export default function Portfolio() {
-  const [selected, setSelected] = useState("featured");
-  const [data, setData] = useState([]);
-  const list = [
-    {
-      id: "all",
-      title: "All project",
-    },
-    {
-      id: "web",
-      title: "Web App",
-    },
-    {
-      id: "design",
-      title: "Personal Project",
-    },
-    {
-      id: "school",
-      title: "School project",
-    },
-  ];
-
-  useEffect(() => {
-    switch (selected) {
-      case "all":
-        setData(allProject);
-        break;
-      case "web":
-        setData(webPortfolio);
-        break;
-      case "design":
-        setData(personalProject);
-        break;
-      case "school":
-        setData(schoolProject);
-        break;
-      default:
-        setData(allProject);
-    }
-  }, [selected]);
-
+const Portfolio = ({img,link}) => {
   return (
-    <div className="portfolio" id="portfolio">
-      <h1>Portfolio</h1>
-      <ul>
-        {list.map((item) => (
-          <PortfolioList
-          title={item.title}
-          active={selected === item.id}
-          setSelected={setSelected}
-          id={item.id}
-          />
-        ))}
-      </ul>
-      <div className="container">
-        {data.map((d) => (
-          <div className="item">
-            <img
-              src={d.img}
-              alt=""
-            />
-            <h3>{d.title}</h3>
-          </div>
-        ))}
+    <div className="p">
+      <div className="p-browser">
+        <div className="p-circle"></div>
+        <div className="p-circle"></div>
+        <div className="p-circle"></div>
       </div>
+      <a href={link} target="_blank" rel="noreferrer">
+        <img src={img} alt="" className="p-img" />
+      </a>
     </div>
   )
 }
+
+export default Portfolio
--- src/components/portfolioList/PortfolioList.jsx
+++ src/components/portfolioList/PortfolioList.jsx
@@ -1,12 +1,30 @@
-import "./portfoliolist.scss";
+import "./portfolioList.scss"
+import Portfolio from "../portfolio/Portfolio";
+import { portfolio } from "../../data";
 
-export default function PortfolioList({ id, title, active, setSelected }) {
+const PortfolioList = () => {
   return (
-    <li
-      className={active ? "portfolioList active" : "portfolioList"}
-      onClick={() => setSelected(id)}
-    >
-      {title}
-    </li>
+    <div className="portfolio" id="portfolio">
+      <div class="max-width">
+        <div class="container">
+          <div className="pl">
+            <h1 class="title">Portfolio</h1>
+              <div className="pl-texts">
+                <h1 className="pl-title">This is my personal project or school project</h1>
+                <p className="pl-desc">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                </p>
+              </div>
+              <div className="pl-list">
+                {portfolio.map(item=>(
+                  <Portfolio key={item.id} img={item.img} link={item.link} />
+                ))}
+              </div>
+            </div>
+        </div>
+      </div>
+    </div>
   );
-}
+};
+
+export default PortfolioList
