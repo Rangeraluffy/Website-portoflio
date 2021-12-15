0413df8b822c0917c33173c3ec138a8ef7cf7968
Alexandre MAROLLEAU <alexandre.marolleau@hotmail.fr>
2021-12-15 17:26:18 +0100

    Contact Form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,27 +1,42 @@
 import { useState } from "react";
+import emailjs from 'emailjs-com';
 import "./contact.scss";
 
 export default function Contact() {
-  const [message, setMessage] = useState(false);
 
-  const handleSubmit = (e) => {
+  function sendEmail(e) {
     e.preventDefault();
-    setMessage(true);
-  };
+
+    emailjs.sendForm('service_mw96vwq', 'template_7j7u1zd', e.target, 'user_G0gHK2FJGn3KFJ9N511Fv')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  }
+
   return (
-    <div className="contact" id="contact">
-      <div className="left">
-        <img src="assets/shake.svg" alt="" />
-      </div>
-      <div className="right">
-        <h2>Contact.</h2>
-        <form onSubmit={handleSubmit}>
-          <input type="text" placeholder="Email" />
-          <textarea placeholder="Message"></textarea>
-          <button type="submit">Send</button>
-          {message && <span>Thanks, I'll reply ASAP :)</span>}
-        </form>
-      </div>
+    // <div className="container">
+    //   <form onSubmit={sendEmail}>
+    //     <label>Name</label>
+    //     <input type="text" name="user_name" />
+    //     <label>Email</label>
+    //     <input type="email" name="user_email" />
+    //     <label>Message</label>
+    //     <textarea name="message" />
+    //     <input type="submit" value="Send" />
+    //   </form>
+    // </div>
+
+    <div className="container">
+      <form onSubmit={sendEmail}>
+      <input name="name" type="text" class="feedback-input" placeholder="Name" />
+      <input name="email" type="text" class="feedback-input" placeholder="Email" />
+      <textarea name="message" class="feedback-input" placeholder="Comment"></textarea>
+      <input type="submit" value="Send"/>
+      </form>
     </div>
+
+
   );
 }
