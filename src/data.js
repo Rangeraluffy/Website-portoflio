9eaa155ab35e87f8279493c0bd9306dcb7aefc7a
Alexandre MAROLLEAU <alexandre.marolleau@hotmail.fr>
2021-12-16 16:06:51 +0100

    Add project portfolio

--- src/data.js
+++ src/data.js
@@ -1,38 +1,85 @@
 export const allPortfolio = [
   {
     id: 1,
-    title: "First Web Site",
-    img: "assets/project.webp",
+    title: "Video Game Web Site",
+    img: "assets/iphone.png",
+    description: "HTML & CSS",
+    formation: "Autodidacte",
   },
   {
     id: 2,
-    title: "Second Web Site",
-    img: "assets/project2.webp",
+    title: "Chablais Property",
+    img: "assets/property.png",
+    description: "Ruby On Rails",
+    formation: "Autodidacte",
   },
   {
     id: 3,
-    title: "Third Web Site",
-    img: "assets/project3.webp",
+    title: "Pro Tips",
+    img: "assets/pro_tips.png",
+    description: "Ruby On Rails",
+    formation: "School Captstone Project",
   },
   {
     id: 4,
-    title: "Third Web Site",
-    img: "assets/project3.webp",
+    title: "Covid Web Site",
+    img: "assets/covid_website.png",
+    description: "HTML & CSS",
+    formation: "School Captstone Project",
+  },
+  {
+    id: 5,
+    title: "Project 2 Javascript",
+    img: "assets/javascript_project.png",
+    description: "Javascript",
+    formation: "School Captstone Project",
+  },
+  {
+    id: 5,
+    title: "Project 3 Ruby on Rails",
+    img: "assets/pinterest.png",
+    description: "Ruby On rails",
+    formation: "School Captstone Project",
   },
 ];
 
 export const schoolPortfolio = [
   {
-    id: 1,
-    title: "First Web Site",
-    img: "assets/project.webp",
+    id: 5,
+    title: "Project 2 Javascript",
+    img: "assets/javascript_project.png",
+    description: "Javascript",
+    formation: "School Captstone Project",
+  },
+  {
+    id: 2,
+    title: "Chablais Property",
+    img: "assets/property.png",
+    description: "Ruby On Rails",
+    formation: "Autodidacte",
+    },
+  {
+    id: 3,
+    title: "Pro Tips",
+    img: "assets/pro_tips.png",
+    description: "Ruby On Rails",
+    formation: "School Captstone Project",
   },
 ];
 export const personnalPortfolio = [
   {
+    id: 4,
+    title: "Covid Web Site",
+    img: "assets/covid_website.png",
+    description: "HTML & CSS",
+    formation: "Personnal Project",
+  },
+  {
     id: 1,
-    title: "Third Web Site",
-    img: "assets/project3.webp",
+    title: "First Web Site",
+    img: "assets/iphone.png",
+    description: "HTML & CSS",
+    formation: "Autodidacte",
   },
 ];
 export const webPortfolio = [
