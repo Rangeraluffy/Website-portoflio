78e455950774fdce236274bb865ed71b66b889bf
Alexandre MAROLLEAU <alexandre.marolleau@hotmail.fr>
2022-01-03 16:20:12 +0100

    Update index and data js

---
+++ src/data.jsx
@@ -0,0 +1,165 @@
+const allPortfolio = [
+  {
+    id: 1,
+    category: 'school',
+    img: 'assets/property.png',
+    tools: 'Ruby On Rails',
+    url: 'https://chablais-property.herokuapp.com/',
+    gitHub: 'https://github.com/Rangeraluffy/capstone-project-wad-c5-s2-4493-2439',
+    languages: {
+      fr: {
+        title: 'Chablais Immobilier',
+        description: "Il s'agit de mon projet final pour compléter le programme de développement d'applications Web. Mon objectif est de mettre en valeur les connaissances et les compétences que j'ai développées tout au long des quatres cours de ce programme. Celui-ci était la dernière étape pour compléter le programme complet de l'EPFL Extension School. Après une présentation devant un jury, celui ci a été validé et félicité.",
+        formation: 'Project Final , Présenter a un Jury',
+        firstTitleModal: 'Aperçu du projet',
+        firstDescription: "Peillex Immobilier a été imaginé comme une refonte d'une agence immobilière déjà existante sur le marché du Chablais. Donner un nouveau style à un marché très concurrentiel. L'administrateur peut créer des comptes pour les agents qui travaillent dans cette agence et ainsi publier des actualités liées au marché immobilier ou simplement de nouveaux mandats pour la vente de maisons ou d'appartements.",
+        projectGoal: 'Objectifs du projet',
+        projectGoal1: 'Front-end and back-end',
+        projectGoal2: 'Third party software and services',
+        projectGoal3: 'General programming techniques',
+        projectGoal4: 'General programming techniques'
+      },
+      en: {
+        title: 'Chablais Property',
+        description: 'This is the Capstone Project for the Web Application Development Program. As a final step to complete the full program of EPFL Extension School.',
+        formation: 'Final Project, Present to a Jury',
+        firstTitleModal: 'Project overview',
+        firstDescription: "'Peillex Real Estate' was imagined like a redesign of an already existing real estate agency on the Chablais market. To give a new style with a very competitive market. The administrator can create accounts for agents who work in this agency and thus published news related to the real estate market or simply new mandates for the sale of houses or apartments.",
+        projectGoal: 'Project goals',
+        projectGoal1: 'Front-end and back-end',
+        projectGoal2: 'Third party software and services',
+        projectGoal3: 'General programming techniques',
+        projectGoal4: 'General programming techniques'
+      }
+    }
+  }, {
+    id: 2,
+    category: 'personal',
+    img: 'assets/covid_website.png',
+    tools: 'HTML & CSS',
+    gitHub: 'https://github.com/Rangeraluffy/COVID-Website',
+    languages: {
+      fr: {
+        title: 'Covid Web Site',
+        description: "Il s'agit d'un projet personnel réalisé le soir sur mon temps libre pour améliorer mes compétences en CSS pur.",
+        formation: 'Autodidacte',
+        firstTitleModal: 'Aperçu du projet',
+        firstDescription: 'Le HTML était déjà présent dans le code le principe étant avec les images fournies dans le fichier de faire simplement tout le site en CSS sans toucher au HTML.',
+        projectGoal: 'Objectifs du projet',
+        projectGoal1: 'Front-end and back-end',
+        projectGoal2: 'Third party software and services',
+        projectGoal3: 'General programming techniques'
+      },
+      en: {
+        title: 'Covid Web Site',
+        description: 'This is a personal project carried out in the evening on my free time to improve my CSS skills. This project was developed by Alexandre Marolleau.',
+        formation: 'Autodidacte',
+        firstTitleModal: 'Project overview',
+        firstDescription: 'The HTML was already present in the code the principle being with the images provided in the file to simply make the whole website in CSS without touching the HTML.',
+        projectGoal: 'Objectifs du projet',
+        projectGoal1: 'Front-end and back-end',
+        projectGoal2: 'Third party software and services',
+        projectGoal3: 'General programming techniques'
+      }
+    }
+  }, {
+    id: 3,
+    category: 'school',
+    img: 'assets/pro_tips.png',
+    tools: 'Ruby On Rails',
+    url: 'https://sheltered-tor-87333.herokuapp.com',
+    gitHub: 'https://github.com/Rangeraluffy/project-wad-c4-s10-4493-2439',
+    languages: {
+      fr: {
+        title: 'Pro Tips',
+        description: "Il s'agit du projet du cours Applications Web dynamiques. Le troisième projet de cours de l'EPFL Extension School sur le programme Web application development program.",
+        formation: 'School',
+        firstTitleModal: 'Aperçu du projet',
+        firstDescription: "L'application est un forum de conseils, appelé Pro Tips, qui permet aux développeurs de s'inscrire et de partager de courts conseils entre eux, puis de commenter les conseils que d'autres ont créés. Il est possible de se connecter en tant qu'Administrateur et de gérer les utilisateurs, commentaires, tips.",
+        projectGoal: 'Objectifs du projet'
+      },
+      en: {
+        title: 'Pro Tips',
+        description: 'This is the Dynamic web applications course project. The third course project of the EPFL Extension School about the program Web application development program.',
+        formation: 'School',
+        firstTitleModal: 'Project overview',
+        firstDescription: 'The application is a tips forum, called Pro Tips, that allows developers to register and share short bits of advice with each other, and then comment on the tips others have created. It is possible to connect as Administrator and manage users, comments, tips.',
+        projectGoal: 'Project goals'
+      }
+    }
+  }, {
+    id: 4,
+    category: 'personal',
+    img: 'assets/iphone.png',
+    tools: 'React',
+    gitHub: 'https://github.com/Rangeraluffy/Website-portoflio',
+    languages: {
+      fr: {
+        title: 'Personnel Portfolio',
+        description: 'Je suis en francais',
+        formation: 'Je suis en francais',
+        firstTitleModal: 'Aperçu du projet',
+        firstDescription: 'Je suis en francais',
+        projectGoal: 'Objectifs du projet'
+      },
+      en: {
+        title: 'Personnal Portfolio',
+        description: 'This is a personal project carried out in the evening on my free time to improve my CSS skills. This project was developed by Alexandre Marolleau.',
+        formation: 'Free Times',
+        firstTitleModal: 'Project overview',
+        firstDescription: 'The HTML was already present in the code the principle being with the images provided in the file to simply make the whole website in CSS without touching the HTML.',
+        projectGoal: 'Project goals'
+      }
+    }
+  }, {
+    id: 5,
+    category: 'school',
+    img: 'assets/javascript_project.png',
+    tools: 'Javascript',
+    gitHub: 'https://github.com/Rangeraluffy/project-wad-c2-s4-4493-2439',
+    languages: {
+      fr: {
+        title: 'Project 2 Javascript',
+        description: "Le site Web est en HTML, CSS et avec des images statiques ; mon travail consistait à faire fonctionner ces pages Web statiques de manière dynamique avec différentes techniques JavaScript.",
+        formation: 'School',
+        firstTitleModal: 'Aperçu du projet',
+        firstDescription: "L'objectif du site est de permettre aux visiteurs de visualiser les accessoires vestimentaires par catégorie (chapeaux, gants, chaussettes et lunettes de soleil), de filtrer par couleur, puis d'ajouter des articles à leur panier. La liste de souhaits peut ensuite être consultée et des éléments peuvent être supprimés de la liste de souhaits",
+        projectGoal: 'Objectifs du projet'
+      },
+      en: {
+        title: 'Project 2 Javascript',
+        description: 'The website is static HTML, CSS, and images; my job was to make these static web pages work dynamically with different JavaScript techniques.',
+        formation: 'School',
+        firstTitleModal: 'Project overview',
+        firstDescription: 'The aim of the site is to allows visitors to view clothing accessories by category (hats, gloves, socks, and sunglasses), filter by color, and then add up to three items to their own wishlist. The wishlist can then be viewed, and items can be removed from the wishlist.',
+        projectGoal: 'Project goals'
+      }
+    }
+  }, {
+    id: 6,
+    category: 'school',
+    img: 'assets/pinterest.png',
+    tools: 'Javascript',
+    gitHub: 'https://github.com/Rangeraluffy/project-wad-c3-s10-4493-2439',
+    languages: {
+      fr: {
+        title: 'Project 3 Ruby on Rails',
+        description: "Le projet 3 était le premier projet sous Ruby On rails : il s'agissais d'évaluer mes capacité à interpréter le résumé du projet et à créer les modèles, contrôleurs, vues et itinéraires appropriés. En particulier, la soumission du projet doit démontrer tous les aspects du développement avec le framework Ruby on Rails enseigné dans mon cours.",
+        formation: 'School',
+        firstTitleModal: 'Aperçu du projet',
+        firstDescription: "Créez une application qui permet aux utilisateurs de créer un tableau d'affichage virtuel pour les images et les photos qu'ils trouvent en ligne, de rechercher, épingler , se connecter, ajouter des commentaires. Ce sera une expérience similaire mais plus basique au site Web populaire Pinterest.",
+        projectGoal: 'Objectifs du projet'
+      },
+      en: {
+        title: 'Project 3 Ruby on Rails',
+        description: "The outlines above don't go into as much detail as previous course projects: this is to assess your ability to interpret the project brief and create the appropriate models, controllers, views, and routes based on the guidelines below. In particular, the project submission must demonstrate all aspects of developing with the Ruby on Rails framework taught in this course.",
+        formation: 'School',
+        firstTitleModal: 'Project overview',
+        firstDescription: 'Create an application that allows people to create a virtual pinboard for images and photos that they find online, search, pin, connect, add comments. It will be a similar but more basic experience to the popular website Pinterest website.',
+        projectGoal: 'Project goals'
+      }
+    }
+  }
+];
+
+export default allPortfolio;
---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+import React, { Suspense } from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './i18next';
+
+ReactDOM.render(
+  <Suspense fallback={<div>Loading ~~~</div>}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Suspense>,
+  document.getElementById('root')
+);
