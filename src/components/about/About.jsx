1be64d1a5b1118f97229daed1bf175622cf72516
Alexandre MAROLLEAU <alexandre.marolleau@hotmail.fr>
2022-01-04 16:31:12 +0100

    Translate Section About with i18next remove Use Effect and add some css

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,43 +1,27 @@
 import './about.scss';
 import { init } from 'ityped';
-import React, { useEffect, useRef } from 'react';
+import React, { useRef } from 'react';
 import { useTranslation } from 'react-i18next';
 
 export default function About() {
   const { t } = useTranslation();
 
-  const textRef = useRef();
-
-  useEffect(() => {
-    init(textRef.current, {
-      showCursor: true,
-      backDelay: 1500,
-      backSpeed: 40,
-      strings: [t('Intro.1'), 'Looking for a new', 'Job']
-    });
-  }, [t]);
-
   return (
     <div className="about" id="about">
       <div className="max-width">
         <div className="container">
-          <h1 className="title">About me</h1>
+          <h1 className="title">{t('About.1')}</h1>
           <div className="about-content">
             <div className="column left">
               <img src="assets/about.png" alt="" />
             </div>
             <div className="column right">
               <div className="text">
-                I&apos;m Alexandre and I
-                <span ref={textRef} />
+                {t('About.2')}
+                <span>{t('About.3')}</span>
               </div>
               <p>
-                Hello there my Name is Alexandre Marolleau I am 26 years. Originaly from
-                Haute-Savoie. I looking a new opportunity in Web developmen.Holder of a Bachelor in
-                Game Design acquired at Bellecour Ecole (Lyon – France), Recently graduated at the
-                Ecole Polytechnique Fédérale de Lausanne (EPFL) Extension School with a
-                specialisation in web development application to obtain my Certificate of Open
-                Studies (COS).
+                {t('About.4')}.
               </p>
               <a href="/assets/CV_Alexandre_Marolleau_EN.pdf" target="_blank">Download CV</a>
             </div>
